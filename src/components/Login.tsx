f95ec48f5c6558f3704951b0a502e4ca8ebeb38a
kimdonggu42 <kdonggoo44@gmail.com>
2023-06-06 18:28:38 +0900

    Feat : Login, Signup 기본 레이아웃 추가

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,22 +1,55 @@
 import styled from "styled-components";
+import { useState } from "react";
 
-const Test = styled.div`
-  display: flex;
+const FormContainer = styled.form`
   font-size: 1rem;
-  margin-top: 5rem;
+`;
 
-  > p {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+const FieldsetArea = styled.fieldset`
+  border: 1px solid gray;
+
+  > input {
+    border: 1px solid gray;
+  }
+
+  > button {
+    margin-left: 1rem;
+    border: 1px solid gray;
   }
 `;
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleData = (e: any) => {
+    if (e.target.type === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.type === "password") {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <Test>
-      <p>일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십</p>
-    </Test>
+    <FormContainer onSubmit={handleSubmit}>
+      <FieldsetArea>
+        <legend>로그인</legend>
+        <input type='email' placeholder='email' required onChange={handleData} value={email} />
+        <input
+          type='password'
+          placeholder='password'
+          required
+          onChange={handleData}
+          value={password}
+        />
+        <button type='submit'>로그인</button>
+      </FieldsetArea>
+    </FormContainer>
   );
 }
 
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,5 +1,66 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+const FormContainer = styled.form`
+  font-size: 1rem;
+`;
+
+const FieldsetArea = styled.fieldset`
+  border: 1px solid gray;
+
+  > input {
+    border: 1px solid gray;
+  }
+
+  > button {
+    margin-left: 1rem;
+    border: 1px solid gray;
+  }
+`;
+
 function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleData = (e: any) => {
+    if (e.target.type === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.type === "password") {
+      setPassword(e.target.value);
+    } else if (e.target.type === "text") {
+      setDisplayName(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <FormContainer onSubmit={handleSubmit}>
+      <FieldsetArea>
+        <legend>회원가입</legend>
+        <input
+          type='text'
+          placeholder='nickname'
+          required
+          onChange={handleData}
+          value={displayName}
+        />
+        <input type='email' placeholder='email' required onChange={handleData} value={email} />
+        <input
+          type='password'
+          placeholder='password'
+          required
+          onChange={handleData}
+          value={password}
+        />
+        <button type='submit'>회원가입</button>
+      </FieldsetArea>
+    </FormContainer>
+  );
 }
 
 export default Signup;
