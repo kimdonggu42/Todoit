7e1d9c1eb7a5071f76d7a8631a77940bed819a82
kimdonggu42 <kdonggoo44@gmail.com>
2023-06-19 15:10:32 +0900

    Feat : 할 일 수정 시 유효성 검사 및 alert 추가

--- src/components/EditTodoModal.tsx
+++ src/components/EditTodoModal.tsx
@@ -1,28 +1,37 @@
 import * as AddTodoModal from "./AddTodoModal";
 import { useState } from "react";
 import { useFireStore } from "../hooks/useFirestore";
+import { toast } from "react-toastify";
 
 function EditTodoModal({ list, editModalOpen, setEditModalOpen }: any) {
-  const [editTodoContent, setEditTodoContent] = useState<string>(list.todoBody);
-  const [editTodoCreatedAt, setEditTodoCreatedAt] = useState<string>(list.todoDate);
+  const [editTodoBody, setEditTodoBody] = useState<string>(list.todoBody);
+  const [editTodoDate, setEditTodoDate] = useState<string>(list.todoDate);
 
   const { updateDocument } = useFireStore("todo");
 
   // firestore 데이터 수정
   const editTodoPatch = async (id: string) => {
-    const updatedFields = {
-      todoBody: editTodoContent,
-      todoDate: editTodoCreatedAt,
-    };
-    updateDocument(id, updatedFields);
+    if (editTodoBody && editTodoDate) {
+      const updatedFields = {
+        todoBody: editTodoBody,
+        todoDate: editTodoDate,
+      };
+      updateDocument(id, updatedFields);
+      toast.success("할 일이 수정되었습니다.");
+      setEditModalOpen(!editModalOpen);
+    } else if (editTodoBody.length === 0) {
+      toast.error("할 일을 입력해주세요.");
+    } else if (editTodoDate.length === 0) {
+      toast.error("완료 기한을 선택해주세요.");
+    }
   };
 
   const changeEditContent = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditTodoContent(e.target.value);
+    setEditTodoBody(e.target.value);
   };
 
   const changeEditCreatedAt = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditTodoCreatedAt(e.target.value);
+    setEditTodoDate(e.target.value);
   };
 
   const openEditModal = () => {
@@ -36,14 +45,14 @@ function EditTodoModal({ list, editModalOpen, setEditModalOpen }: any) {
         <AddTodoModal.ContentInput
           className='content-input'
           type='text'
-          value={editTodoContent}
+          value={editTodoBody}
           placeholder='할 일을 입력해주세요'
           onChange={changeEditContent}
         />
         <AddTodoModal.DateInput
           className='createdAt-input'
           type='date'
-          value={editTodoCreatedAt}
+          value={editTodoDate}
           onChange={changeEditCreatedAt}
         />
         <AddTodoModal.AddModalButtonArea>
@@ -51,7 +60,6 @@ function EditTodoModal({ list, editModalOpen, setEditModalOpen }: any) {
             className='submit-button'
             onClick={() => {
               editTodoPatch(list.id);
-              openEditModal();
             }}
           >
             수정
